f103e74b694264fc7940438b9a589fd54c97eac6
jinghewang <jinghewang@163.com>
2016-05-11 23:02:17 +0800

    button 调整

--- app/src/main/java/com/example/hbd/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hbd/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,21 +13,31 @@ public class MainActivity extends AppCompatActivity {
     private Button btn;
     private  TextView textView;
     private int count =0;
+    Toast toast= null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView)findViewById(R.id.textView2);
         btn = (Button)this.findViewById(R.id.button1);
     }
 
 
     public void Btn1_Click(View view)
     {
-        String str = "---->" + count;
-        textView.setText(str);
+        String str = "count:" + count;
+        toast=Toast.makeText(this.getApplicationContext(), str, Toast.LENGTH_SHORT);
+        toast.show();
+        count++;
+    }
+
+    public void Btn2_Click(View view)
+    {
+        String str = "count2:" + count;
+        //textView.setText(str);
+        toast=Toast.makeText(this.getApplicationContext(), str, Toast.LENGTH_SHORT);
+        toast.show();
         count++;
     }
 }
